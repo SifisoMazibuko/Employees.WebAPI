a732598bfc7aa3e4c52b28b157c8faa53b950884
sifisomazibuko <sifisomazibbuko@hotmail.com>
2019-02-06 13:48:02 +0200

    initial commit

---
+++ Employees.Interface/IEmployeeDataService.cs
@@ -0,0 +1,18 @@
+﻿using Employees.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employees.Interface
+{
+    public interface IEmployeeDataService
+    {
+        List<Employee> GetAllEmployees();
+        Employee GetEmployeebyId(string employeeId);
+        List<Employee> SaveEmployee(Employee employee);
+        List<Employee> DeleteEmployeeById(string eid);
+        List<Employee> UpdateEmployee(string eid, Employee employee);
+    }
+}
---
+++ Employees.Model/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employees.Model
+{
+    public class Employee
+    {
+        public string EmployeeID { get; set; }
+        public string EmployeeName { get; set; }
+        public string ContactNumber { get; set; }
+        public string MailID { get; set; }
+        public string Designation { get; set; }
+        public string SkillSet { get; set; }
+        public string ReportingPerson { get; set; }
+        public bool IsSupervisor { get; set; }
+    }
+}
